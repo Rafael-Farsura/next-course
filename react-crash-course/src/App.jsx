e2414137f2c9b7a8500ff176b7124ba4816650e3
Rafael Farsura <rafaelfarsura@gmail.com>
2024-08-19 21:31:56 -0300

    Initial commit

---
+++ react-crash-course/src/App.jsx
@@ -0,0 +1,29 @@
+import PostsList from "./components/PostList.jsx";
+import MainHeader from "./components/MainHeader.jsx";
+import {useState} from "react";
+
+
+function App() {
+    const [modalIsVisible, setModalIsVisible] = useState(true)
+
+    function showModalHandler() {
+        setModalIsVisible(true);
+    }
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
+
+    return (
+        <>
+            <MainHeader onCreatePost={showModalHandler} />
+            <main>
+                <PostsList
+                    isPosting={modalIsVisible}
+                    onStopPosting={hideModalHandler}
+                />
+            </main>
+        </>
+    );
+}
+
+export default App
